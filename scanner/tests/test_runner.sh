2ee70589360d1e298b75dbc404993dff7290df62
Carlos Westermann <westermanncarloseduardo@gmail.com>
2023-10-22 18:12:08 -0300

    reorganizing and adding some test cases

---
+++ scanner/tests/test_runner.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Move para o diretório pai e compila o scanner usando o Makefile
+cd ..
+make
+cd -
+
+mkdir -p actual_outputs
+
+total_tests=0
+passed_tests=0
+
+report="Test Report\n\n"
+
+for testcase in inputs/*.txt; do
+    total_tests=$((total_tests + 1))
+
+    filename=$(basename $testcase)
+    
+    echo "Running test: $filename"
+    
+    ../scanner < $testcase > actual_outputs/$filename
+    
+    if diff -q expected_output/$filename actual_outputs/$filename; then
+        echo "Test $filename PASSED"
+        report="$report$filename: PASSED\n"
+        
+        passed_tests=$((passed_tests + 1))
+    else
+        echo "Test $filename FAILED"
+        report="$report$filename: FAILED\n"
+    fi
+
+    echo "-------------------------"
+done
+
+report="$report\n$passed_tests tests passed out of $total_tests total tests."
+
+echo -e $report > test_report.txt
+
+cat test_report.txt
